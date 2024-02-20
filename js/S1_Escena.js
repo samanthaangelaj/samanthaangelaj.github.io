791d5461375cdbf5919799809420bade16d2dc41
samanthaangelaj <samantha.r.johansson@gmail.com>
2024-02-21 00:49:16 +0100

    escena 3

--- js/S1_Escena.js
+++ js/S1_Escena.js
@@ -9,8 +9,8 @@
  */
 
 // Modulos necesarios
-//import * as THREE from "../lib/three.module.js";
-//import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import * as THREE from "../lib/three.module.js";
+import {GLTFLoader} from "../lib/GLTFLoader.module.js";
 
 // Variables de consenso
 let renderer, scene, camera;
