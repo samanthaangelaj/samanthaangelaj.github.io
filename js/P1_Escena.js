cf9612a82aed106b06d9b918a5b41d6b80132c88
samanthaangelaj <samantha.r.johansson@gmail.com>
2024-02-21 13:49:53 +0100

    escena formas

--- js/P1_Escena.js
+++ js/P1_Escena.js
@@ -22,6 +22,8 @@ let renderer, scene, camera;
  * TO DO: Variables globales de la aplicacion
  *******************/
 
+let fiveforms;
+
 // Acciones
 init();
 loadScene();
@@ -32,10 +34,12 @@ function init()
     // Motor de render
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
+
     /*******************
     * TO DO: Completar el motor de render y el canvas
     *******************/
-    //document.getElementById('container').appendChild( renderer.domElement );
+
+    document.getElementById('container').appendChild( renderer.domElement );
    
     // Escena
     scene = new THREE.Scene();
@@ -49,13 +53,13 @@ function init()
 
 function loadScene()
 {
-    const material = new THREE.MeshBasicMaterial({color: 'green', wireframe: true} );
+    var materialfloor = new THREE.MeshBasicMaterial({color: 'green', wireframe: true} );
 
     /*******************
     * TO DO: Construir un suelo en el plano XZ
     *******************/
 
-    const floor = new THREE.MeshNormalMaterial(new THREE.CircleGeometry(10,40), material);
+    const floor = new THREE.MeshNormalMaterial(new THREE.CircleGeometry(10,40), materialfloor);
     floor.rotation.x = -Math.PI/3
     scene.add(floor); 
 
@@ -63,14 +67,55 @@ function loadScene()
     * TO DO: Construir una escena con 5 figuras diferentes posicionadas
     * en los cinco vertices de un pentagono regular alredor del origen
     *******************/
+    
+    const materialforms = new THREE.MeshNormalMaterial();
+
+    const geometry1 = new THREE.CylinderGeometry(0, radius, height, 4, 1)
+    const pyramid = new THREE.Mesh(geometry1, materialforms);
+    scene.add(pyramid);
+
+    const geometry2 = new THREE.SphereGeometry(1,20,20);
+    const sphere = new THREE.Mesh(geometry2, materialforms); 
+    scene.add(sphere); 
+
+    const geometry3 = new THREE.CylinderGeometry(); 
+    const cylinder = new THREE.Mesh(geometry3, material); 
+    scene.add(cylinder); 
+
+    const geometry4 = new THREE.BoxGeometry(2,2,2); 
+    const box = new THREE.Mesh(geometry4, material); 
+    scene.add(box); 
+
+    const geometry5 = new THREE.DodecahedronGeometry(); 
+    const dodeca = new THREE.Mesh(geometry5, material); 
+    scene.add(dodeca); 
 
     /*******************
     * TO DO: Añadir a la escena un modelo importado en el centro del pentagono
     *******************/
 
+    //Tree, cocunut pero no esta funcionando hacer una cuenta en clara.io
+
     /*******************
     * TO DO: Añadir a la escena unos ejes
     *******************/
+
+    fiveforms = new THREE.Object3D(); 
+    fiveforms.position.y = 1.0;
+    //pyramid.position.y = 1.5;
+    pyramid.position(1,1.5);
+    sphere.position(2,2);
+    cylinder.position(3,1);
+    box.position(1.5,2);
+    dodeca.position(4,2);
+
+    box.add(new THREE.AxesHelper(1)); 
+
+    scene.add(fiveforms); 
+    fiveforms.add(pyramid,spehere,cylinder,box,dodeca); 
+
+    scene.add(new THREE.AxesHelper(5));
+
 }
 
 function update()
@@ -79,6 +124,10 @@ function update()
     * TO DO: Modificar el angulo de giro de cada objeto sobre si mismo
     * y del conjunto pentagonal sobre el objeto importado
     *******************/
+
+
+
+
 }
 
 function render()
