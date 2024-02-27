f196cae97692e09752e6f4153eb7de8d1f540e2e
samanthaangelaj <samantha.r.johansson@gmail.com>
2024-02-28 00:45:07 +0100

    floor round 10

--- js/projecto_producto.js
+++ js/projecto_producto.js
@@ -27,7 +27,7 @@ function init() {
     // Instanciar la camara
     camera= new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
     camera.position.set(0.5,2,7);
-    cameraControls = new OrbitControls( camera, renderer.domElement );
+    cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
     cameraControls.target.set(0,1,0);
     camera.lookAt(0,1,0);      
 
