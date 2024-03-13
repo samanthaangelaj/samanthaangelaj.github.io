0d8d0496cf839b2534d6d62b21df2e6a6e211706
samanthaangelaj <samantha.r.johansson@gmail.com>
2024-03-13 10:15:44 +0100

    last adjust 2

--- js/projecto_producto.js
+++ js/projecto_producto.js
@@ -219,9 +219,31 @@ function loadScene() {
         text5.position.set(-10, -7, 7); 
         scene.add(text5); 
 
+        // Wrap text around a cylinder
+        const curve = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(radius, 0, 0),
+            new THREE.Vector3(radius, height, 0),
+            new THREE.Vector3(-radius, height, 0),
+            new THREE.Vector3(-radius, 0, 0),
+            new THREE.Vector3(radius, 0, 0)
+        ]);
+
+        const points = curve.getPoints(curveSegments);
+
+        for (let i = 0; i < points.length; i++) {
+            textMeshfloor.geometry.vertices[i].x = points[i].x;
+            textMeshfloor.geometry.vertices[i].y = points[i].y;
+            textMeshfloor.geometry.vertices[i].z = points[i].z;
+        }
+
+        return textMeshfloor;
+        // Usage
+        const textMeshfloor = createRoundedText("Baja Beach", 10, 0, 100, 0.03, 0.04, 8); // Adjust radius and height as needed
+        textMeshfloor.position.set(5, 0, 0); // Set position as needed
+        floor.add(textMeshfloor);
     });
 
-    scene.add(new THREE.AxesHelper(3));
+    //scene.add(new THREE.AxesHelper(3));
 
     // Loading 3D models
     const glloader = new THREE.GLTFLoader();
