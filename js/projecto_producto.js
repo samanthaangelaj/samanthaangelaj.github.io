1860c881244131ada77066916a91a2ebbbef7326
samanthaangelaj <samantha.r.johansson@gmail.com>
2024-03-13 13:29:40 +0100

    patitoo final

--- js/projecto_producto.js
+++ js/projecto_producto.js
@@ -38,7 +38,7 @@ function init() {
     cameraControls = new THREE.OrbitControls( camera, renderer.domElement );
     cameraControls.target.set(0,1,0);
     cameraControls.minDistance = 10; //limit zoom in
-    cameraControls.maxDistance = 30; //limit zoom out 
+    cameraControls.maxDistance = 50; //limit zoom out 
     camera.lookAt(0,1,0); 
     
     // Ambient light
